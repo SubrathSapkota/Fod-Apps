674cf24c8f3e15663b11929f6ac8d9767643f90b
Subrath sapkota <subrathsapkota119@gmail.com>
2023-06-29 15:32:51 +0545

    updated

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -4,7 +4,7 @@ import calsses from './MealsSummary.module.css'
 const MealsSummary = () => {
    return (
       <section className={calsses.summary}>
-         <h2>Delicious Food, Delivered To You</h2>
+         <h2>Delicious Food, Delivered To You,TRY</h2>
          <p> Choose your favourite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home.</p>
          <p> All our meals are cooked with high-quality ingredents , just-in-time and of course by expericenced chefs!</p>
       </section>
